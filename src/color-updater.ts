1b76df0d5e4f4f7c064de8c2069ed3fc0f61c924
GooseOb <gooose.ob@gmail.com>
2024-11-01 19:56:40 +0100

    refactor color updater

--- src/color-updater.ts
+++ src/color-updater.ts
@@ -3,33 +3,39 @@ import { RGB, ReadonlyRGB } from "./types";
 const rgbToHex = (rgb: ReadonlyRGB): string =>
   rgb.reduce((acc, c) => acc + (c > 15 ? "" : "0") + c.toString(16), "#");
 
-type ColorSetter = (colorHEX: string) => void;
-
 export class SmoothColorUpdater {
-  constructor(color: RGB, setColor: ColorSetter) {
+  constructor(
+    color: SmoothColorUpdater["color"],
+    onColorChange: SmoothColorUpdater["onColorChange"],
+  ) {
     this.color = color;
-    this.setColor = setColor;
+    this.onColorChange = onColorChange;
   }
 
   public set(color: ReadonlyRGB, frames = 60) {
-    if (this.colorChanging) cancelAnimationFrame(this.colorChanging);
+    if (this.colorChanging) {
+      cancelAnimationFrame(this.colorChanging);
+    }
 
     const delta: RGB = [0, 0, 0];
-    for (let i = 0; i < 3; i++) delta[i] = (this.color[i] - color[i]) / frames;
+    for (let i = 0; i < 3; i++) {
+      delta[i] = (this.color[i] - color[i]) / frames;
+    }
 
     const targetHEX = rgbToHex(color);
 
     const changeColor = () => {
       for (let i = 0; i < 3; i++) this.color[i] -= delta[i];
-      const currHEX = rgbToHex(this.color.map((num) => Math.round(num)) as RGB);
-      if (currHEX !== targetHEX)
+      const currHEX = rgbToHex(this.color.map(Math.round) as RGB);
+      if (currHEX !== targetHEX) {
         this.colorChanging = requestAnimationFrame(changeColor);
-      this.setColor(currHEX);
+      }
+      this.onColorChange(currHEX);
     };
     this.colorChanging = requestAnimationFrame(changeColor);
   }
 
-  private setColor: ColorSetter;
+  private onColorChange: (colorHEX: string) => void;
   private color: RGB;
   private colorChanging?: number;
 }
